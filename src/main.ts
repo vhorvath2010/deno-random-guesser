c517b3bba548a3959bb1321c2097f95573e86d5a
Vince <vhorvath2010@gmail.com>
2024-11-13 23:21:50 -0500

    add random number gen

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
-console.log('hello deno!');
+import { randomIntegerBetween } from "@std/random";
+
+const specialNumber = randomIntegerBetween(1, 100);
+console.log(`Hey kid, between you and me, the secret number is ${specialNumber}`);
